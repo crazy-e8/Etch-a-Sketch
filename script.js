ebd2b0ee1399845c36eb1912728fbead56c89fc0
Lidor Cohen <lidorcohen.p1@gmail.com>
2024-04-26 17:58:35 +0300

    implemented neon style buttons and grid size slider

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
 const drawingGridDiv = document.getElementById("drawing-grid");
+const sizeSlider = document.getElementById("grid-size-slider");
+const sizeLabel = document.getElementById("grid-size-label");
+const DRAWING_GRID_WIDTH = parseInt(getComputedStyle(drawingGridDiv).width);
 const MIN_GRID_SIZE = 10;
 const MAX_GRID_SIZE = 50;
-console.log(drawingGridDiv.style);
-const DRAWING_GRID_WIDTH = parseInt(getComputedStyle(drawingGridDiv).width);
 
 let gridSize = MIN_GRID_SIZE;
 
 function initDrawingGrid() {
   let squareSize = DRAWING_GRID_WIDTH / gridSize;
-  console.log(squareSize);
 
+  drawingGridDiv.innerHTML = "";
   for (let i = 0; i < gridSize; i++) {
     for (let j = 0; j < gridSize; j++) {
       let blankSquare = document.createElement("div");
       blankSquare.style.backgroundColor = "white";
       blankSquare.style.width = squareSize.toString() + "px";
       blankSquare.style.height = squareSize.toString() + "px";
+      blankSquare.style.border = "1px solid black";
       drawingGridDiv.appendChild(blankSquare);
     }
   }
 }
 
+function updateGridSize() {
+  gridSize = parseInt(this.value);
+  console.log(gridSize);
+  initDrawingGrid();
+}
+
+sizeSlider.value = 0;
 initDrawingGrid();
+
+sizeSlider.addEventListener("input", updateGridSize, false);
