cc93ef197e9d8614fbd4f386704e137b40dd4f24
Lidor Cohen <lidorcohen.p1@gmail.com>
2024-04-26 20:17:58 +0300

    fixed grid overflowing tiles when resized

--- script.js
+++ script.js
@@ -9,14 +9,16 @@ let gridSize = MIN_GRID_SIZE;
 
 function initDrawingGrid() {
   let squareSize = DRAWING_GRID_WIDTH / gridSize;
+  console.log(squareSize);
 
   drawingGridDiv.innerHTML = "";
   for (let i = 0; i < gridSize; i++) {
     for (let j = 0; j < gridSize; j++) {
       let blankSquare = document.createElement("div");
       blankSquare.style.backgroundColor = "white";
-      blankSquare.style.width = squareSize.toString() + "px";
-      blankSquare.style.height = squareSize.toString() + "px";
+      blankSquare.style.width = squareSize - 0.1 + "px";
+      blankSquare.style.height = squareSize - 0.1 + "px";
+      blankSquare.style.border = "1px solid lightgray";
       drawingGridDiv.appendChild(blankSquare);
     }
   }
@@ -24,7 +26,6 @@ function initDrawingGrid() {
 
 function updateGridSize() {
   gridSize = parseInt(this.value);
-  console.log(this.value);
   initDrawingGrid();
 }
 
