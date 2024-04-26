15010ba936635456f4264f5ef75b33e000c32b04
Lidor Cohen <lidorcohen.p1@gmail.com>
2024-04-26 20:33:24 +0300

    implemented toggle tiles button

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 const drawingGridDiv = document.getElementById("drawing-grid");
 const sizeSlider = document.getElementById("grid-size-slider");
 const sizeLabel = document.getElementById("grid-size-label");
+const toggleTilesButton = document.getElementById("toggle-tiles");
 const DRAWING_GRID_WIDTH = parseInt(getComputedStyle(drawingGridDiv).width);
 const MIN_GRID_SIZE = 10;
 const MAX_GRID_SIZE = 50;
 
 let gridSize = MIN_GRID_SIZE;
+let tiles = false;
+
+function toggleTiles() {
+  tiles = !tiles;
+
+  if (tiles) {
+    toggleTilesButton.style.backgroundColor = "hsl(317 100% 54%)";
+    toggleTilesButton.style.color = "white";
+  } else {
+    toggleTilesButton.style.backgroundColor = "white";
+    toggleTilesButton.style.color = "hsl(317 100% 54%)";
+  }
+  initDrawingGrid();
+}
 
 function initDrawingGrid() {
   let squareSize = DRAWING_GRID_WIDTH / gridSize;
@@ -18,7 +33,7 @@ function initDrawingGrid() {
       blankSquare.style.backgroundColor = "white";
       blankSquare.style.width = squareSize - 0.1 + "px";
       blankSquare.style.height = squareSize - 0.1 + "px";
-      blankSquare.style.border = "1px solid lightgray";
+      if (tiles) blankSquare.style.border = "1px solid lightgray";
       drawingGridDiv.appendChild(blankSquare);
     }
   }
@@ -33,3 +48,4 @@ sizeSlider.value = 0;
 initDrawingGrid();
 
 sizeSlider.addEventListener("input", updateGridSize, false);
+toggleTilesButton.addEventListener("click", toggleTiles, false);
